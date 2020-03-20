671dce6f61fb59281055a71dc2c83e1aeae794a0
Sandun Isuru Niraj <sandun.isuru@gmail.com>
2020-03-20 19:36:52 +0530

    Global Status Added

---
+++ src/components/Elements/Global/TotalCured.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardContent, Grid, Typography, Avatar } from '@material-ui/core';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import EventIcon from '@material-ui/icons/Event';
+
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        height: '100%'
+    },
+    content: {
+        alignItems: 'center',
+        display: 'flex'
+    },
+    title: {
+        fontWeight: 700
+    },
+    avatar: {
+        backgroundColor: theme.palette.primary.dark,
+        height: 56,
+        width: 56
+    },
+    icon: {
+        height: 32,
+        width: 32
+    },
+    difference: {
+        marginTop: theme.spacing(2),
+        display: 'flex',
+        alignItems: 'center'
+    },
+    differenceIcon: {
+        color: theme.palette.primary.dark,
+    },
+    differenceValue: {
+        color: theme.palette.error.dark,
+        marginRight: theme.spacing(1)
+    }
+}));
+
+function TotalCured(props) {
+
+    const { className } = props;
+
+    const classes = useStyles();
+
+    return (
+        <Card
+            className={clsx(classes.root, className)}
+        >
+            <CardContent>
+                <Grid
+                    container
+                    justify="space-between"
+                >
+                    <Grid item>
+                        <Typography
+                            className={classes.title}
+                            color="textSecondary"
+                            gutterBottom
+                            variant="body2"
+                        >
+                            TOTAL CURED
+            </Typography>
+                        <Typography variant="h3">{props.total_cured}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Avatar className={classes.avatar}>
+                            <EmojiPeopleIcon className={classes.icon} />
+                        </Avatar>
+                    </Grid>
+                </Grid>
+                <div className={classes.difference}>
+                    <EventIcon className={classes.differenceIcon} />
+
+                    <Typography
+                        className={classes.caption}
+                        variant="caption"
+                    >
+                        Last Updated on {props.last_updated}
+                    </Typography>
+                </div>
+            </CardContent>
+        </Card>
+    );
+}
+
+export default TotalCured;
